ba3165bc5b98fef2495cc7896468f303340d8bd4
Gretchen Dziengel <gdziengel@gmail.com>
2011-02-18 12:03:12 -0600

    Added nerdtree, supertab, taglisttoo, and markdown

--- new_setup.sh
+++ new_setup.sh
@@ -1,3 +1,8 @@
 mkdir bundle
 cd bundle
 git clone https://github.com/wgibbs/vim-irblack.git
+git clone https://github.com/tpope/vim-vividchalk.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/tpope/vim-markdown.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/ervandew/taglisttoo.git
