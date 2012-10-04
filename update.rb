92b4f9b4ae513106ed8331cee6feb43842e5278e
Gretchen Dziengel <gdziengel@gmail.com>
2012-10-04 15:41:43 -0500

    Added EverVim https://github.com/kakkyz81/evervim.git

--- update.rb
+++ update.rb
@@ -12,6 +12,7 @@ allfiles = Dir.glob(files)
  
 repos = [
   "https://github.com/tpope/vim-vividchalk.git",
+  "https://github.com/altercation/vim-colors-solarized.git",
   "https://github.com/janx/vim-rubytest.git",
   "https://github.com/ervandew/supertab.git",
   "https://github.com/tpope/vim-markdown.git",
@@ -26,6 +27,7 @@ repos = [
   "https://github.com/davidoc/taskpaper.vim.git",
   "https://github.com/Townk/vim-autoclose.git",
   "https://github.com/tpope/vim-speeddating.git",
+  "https://github.com/kakkyz81/evervim.git",
   "https://github.com/scrooloose/nerdcommenter.git"
 ]
 
